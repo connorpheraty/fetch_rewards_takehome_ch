c57b7a32e6bd9d5bc691d27068f7e10ec91320eb
connorpheraty <46405273+connorpheraty@users.noreply.github.com>
2024-04-11 12:55:57 -0400

    Lint file

--- snowflake_models/brands.sql
+++ snowflake_models/brands.sql
@@ -1,12 +1,12 @@
-select
-    raw_json:_id:"$oid"::string brand_id -- pk
-    ,raw_json:barcode::string barcode
-    ,raw_json:category::string category
-    ,raw_json:categoryCode::string category_code
-    ,raw_json:cpg:"$id":"$oid"::string cpg_id
-    ,raw_json:cpg:"$ref"::string cpg_ref
-    ,raw_json:name::string name
-    ,raw_json:brandCode::string brand_code
-from fetch_ch_takehome.ingestion.brands
-qualify row_number() over (partition by raw_json:_id:"$oid" order by _load_time desc) = 1
+SELECT
+    raw_json:_id:"$oid"::STRING AS brand_id, -- pk
+    raw_json:barcode::STRING AS barcode,
+    raw_json:category::STRING AS category,
+    raw_json:categoryCode::STRING AS category_code,
+    raw_json:cpg:"$id":"$oid"::STRING AS cpg_id,
+    raw_json:cpg:"$ref"::STRING AS cpg_ref,
+    raw_json:name::STRING AS name,
+    raw_json:brandCode::STRING AS brand_code
+FROM fetch_ch_takehome.ingestion.brands
+QUALIFY ROW_NUMBER() OVER (PARTITION BY raw_json:_id:"$oid" ORDER BY _load_time DESC) = 1
 ;
